1392a420cca048b145c6b71e1f8e38ff52aa7a00
Anson416 <lamyiufung2003@gmail.com>
2023-12-01 18:32:03 +0800

    Create collatz_conjecture.py

---
+++ collatz_conjecture.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# File: collatz_conjecture.py
+
+"""
+Given a positive integer n, return a list of positive integers generated from 
+the Collatz conjecture: 
+Repeat the following until 1 is obtained: For even numbers, divide by 2; for 
+odd numbers, multiply by 3 and add 1.
+
+### Usage:
+
+```python
+import collatz_conjecture
+n: int = 10
+generated: list[int] = collatz_conjecture.generate(n)
+```
+"""
+
+import argparse
+
+
+def generate(n: int) -> list[int]:
+    assert n > 0, f"{n} <= 0. n must be a positive integer."
+
+    lst = []
+    while n != 1:
+        n = n // 2 if n % 2 == 0 else 3 * n + 1
+        lst.append(n)
+
+    return lst
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "n", type=int,
+        help="Positive integer",
+    )
+    args = parser.parse_args()
+
+    print(*generate(args.n), sep=", ")
